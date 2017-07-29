754f693c43536614e01e22dcd3709d46398135d3
Sandeep Joseph <sandeep_joseph@hotmail.com>
2017-07-29 20:52:31 +0200

    Initial Upload

---
+++ src/main/java/com/szeptra/SzeptraApplication.java
@@ -0,0 +1,12 @@
+package com.szeptra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzeptraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzeptraApplication.class, args);
+	}
+}
---
+++ src/test/java/com/szeptra/SzeptraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.szeptra;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SzeptraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
